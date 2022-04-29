672e63c933a048e1df878d67ccdfbf88eaea238f
ChiaLinz <93101704+ChiaLinz@users.noreply.github.com>
2022-04-29 02:11:07 -0400

    fix cli test init file

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -6,11 +6,13 @@ def test_request_main_menu_links(client):
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
 
+
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
     assert b"Index" in response.data
+
 
 def test_request_about(client):
     """This makes the index page"""
@@ -18,11 +20,13 @@ def test_request_about(client):
     assert response.status_code == 200
     assert b"About" in response.data
 
+
 def test_request_page1(client):
     """This makes the index page"""
     response = client.get("/welcome")
     assert response.status_code == 200
     assert b"welcome" in response.data
+
 
 def test_request_page_not_found(client):
     """This makes the index page"""
